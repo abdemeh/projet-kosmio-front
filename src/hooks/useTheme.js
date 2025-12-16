5c17393f74ba55f74639ecaeb5c51050234058c5
LF2Zetrei <fernandesl@cy-tech.fr>
2025-12-16 17:22:32 +0100

    finish theme context. add theme context and theme hook

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,1 +1,14 @@
 // Accès au thème courant (dark / light)
+
+import { useContext } from "react"
+import { ThemeContext } from "../context/ThemeContext"
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+
+    if (!context) {
+        throw new Error("useAuth must be used within an ThemeProvider");
+    }
+
+    return context;
+}
