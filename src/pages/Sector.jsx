194c7aea1e21e5caf0032cd071adf9b134abe40e
LF2Zetrei <fernandesl@cy-tech.fr>
2026-02-12 14:57:36 +0100

    add edition button to sector page

--- src/pages/Sector.jsx
+++ src/pages/Sector.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
 import { useMarkdownApi } from "../hooks/useMarkdownApi";
+import { useNavigate } from "react-router-dom";
 
 const Sector = () => {
     const {error, loading, getAllSector} = useMarkdownApi();
-    const [sectors, setSectors] = useState([])
+    const [sectors, setSectors] = useState([]);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -11,12 +14,16 @@ const Sector = () => {
                 const data = await getAllSector();
                 setSectors(data);
             } catch (err) {
-                console.error(err);
+                console.error("Erreur chargement secteurs: ", err);
             }
         };
 
         fetchData();
     }, []);
+
+    const handleEditClick = (id) => {
+        navigate(`/edit/${id}`);
+    };
 
     if (loading) return <p>Chargement des secteurs...</p>;
     if (loading) return <p className="text-red-500">Erreur : {error.message}</p>;
@@ -25,12 +32,24 @@ const Sector = () => {
         <div className="text-center py-20 text-gray-500">
             <h2 className="text-xl font-bold mb-4">Secteurs</h2>
                 {sectors.length === 0 ? (
-                    <p>Aucun secteur trouvé.</p>
+                    <div className="flex flex-col items-center">
+                        <p className="mb-4">Aunc secteur trouvé.</p>
+                        <p className="text-sm text-gray-400">
+                            (Les secteurs apparaîtront ici une fois enregistrées en base de données)
+                        </p>
+                    </div>
                 ) : (
-                    <ul className="space-y-2">
+                    <ul className="space-y-2 max-w-3xl mx-auto px-4">
                     {sectors.map(sector => (
-                        <li key={sector.id} className="p-2 border rounded hover:bg-gray-50">
-                            {sector.title}
+                        <li key={sector.id} className="p-3 border rounded hover:bg-gray-50 flex justify-between items-center bg-white shadow-sm">
+                            <span className="font-medium text-gray-800">{sector.title || "Secteur sans titre"}</span>
+
+                            <button 
+                                onClick={() => handleEditClick(sector.id)}
+                                className="px-3 py-1 text-sm bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors"
+                            >
+                                Éditer
+                            </button>
                         </li>
                     ))}
                     </ul>
