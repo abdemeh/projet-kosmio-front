70cec768a36a8ee0d32a07519d0d6d05fad2d40f
LF2Zetrei <fernandesl@cy-tech.fr>
2026-02-12 15:23:35 +0100

    fix delete button and add delete button in sector page

--- src/pages/Sector.jsx
+++ src/pages/Sector.jsx
@@ -25,6 +25,13 @@ const Sector = () => {
         navigate(`/edit/${id}`);
     };
 
+    const handleDeleteClick = (id) => {
+
+        if (window.confirm("Voulez-vous vraiment supprimer ce secteur ?")) {
+            console.log("Suppression de l'ID :", id);
+        }
+    };
+
     if (loading) return <p>Chargement des secteurs...</p>;
     if (loading) return <p className="text-red-500">Erreur : {error.message}</p>;
 
@@ -40,18 +47,25 @@ const Sector = () => {
                     </div>
                 ) : (
                     <ul className="space-y-2 max-w-3xl mx-auto px-4">
-                    {sectors.map(sector => (
-                        <li key={sector.id} className="p-3 border rounded hover:bg-gray-50 flex justify-between items-center bg-white shadow-sm">
-                            <span className="font-medium text-gray-800">{sector.title || "Secteur sans titre"}</span>
+                        {sectors.map(sector => (
+                            <li key={sector.id} className="p-3 border rounded hover:bg-gray-50 flex justify-between items-center bg-white shadow-sm">
+                                <span className="font-medium text-gray-800">{sector.title || "Secteur sans titre"}</span>
 
-                            <button 
-                                onClick={() => handleEditClick(sector.id)}
-                                className="px-3 py-1 text-sm bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors"
-                            >
-                                Éditer
-                            </button>
-                        </li>
-                    ))}
+                                <button 
+                                    onClick={() => handleEditClick(sector.id)}
+                                    className="px-3 py-1 text-sm bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors"
+                                >
+                                    Éditer
+                                </button>
+
+                                <button
+                                    onClick={() => handleDeleteClick(sector.id)}
+                                    className="px-4 py-2 text-sm font-medium bg-red-500 text-white rounded hover:bg-red-600 transition-colors shadow-sm"
+                                >
+                                    Supprimer
+                                </button>
+                            </li>
+                        ))}
                     </ul>
                 )}
         </div>
